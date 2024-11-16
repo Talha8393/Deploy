e98592a0f007bd4a759f196df88f32feb6c8c71b
Talha Azhar <talhamughal8393@gmail.com>
2024-11-16 18:01:33 +0500

    nov 16

--- src/app/Pricing.js
+++ src/app/Pricing.js
@@ -129,7 +129,8 @@ export default function Pricing() {
     )}    
         </div>
         <div className="card-bottom"> 
-                  </div>
+          
+           </div>
       </>
     </div>
   )
